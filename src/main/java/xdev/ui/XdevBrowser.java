5d4a84965dc299b7fbd9e4edd6cb8aa78389faae
Daniel Leinberger <d.leinberger@xdev-software.de>
2023-01-27 14:00:26 +0100

    Deleted license doc

--- src/main/java/xdev/ui/XdevBrowser.java
+++ src/main/java/xdev/ui/XdevBrowser.java
@@ -1,28 +1,5 @@
 package xdev.ui;
 
-/*-
- * #%L
- * XDEV Application Framework
- * %%
- * Copyright (C) 2003 - 2021 XDEV Software
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Lesser Public License for more details.
- * 
- * You should have received a copy of the GNU General Lesser Public
- * License along with this program.  If not, see
- * <http://www.gnu.org/licenses/lgpl-3.0.html>.
- * #L%
- */
-
-
 import javafx.scene.Scene;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebHistory;
